e75cc51bb27260cde2530556da2552cbe17a2ad1
Pranay Kumar Gupta <pranaykgupta21@gmail.com>
2021-11-15 10:20:00 +0530

    added code for election transaction

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,26 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    string public candidate;
+    //Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //store candidates
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+    //fetch candidate
+    
     //Constructor
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("candidate 1");
+        addCandidate("candidate 2");
     }
+
+    function addCandidate(string memory _name) private{
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
 }
